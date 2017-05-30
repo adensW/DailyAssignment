bdaa2f0e206a10a641f27441aeadcd8f761da520
adensW <1012790248@qq.com>
2017-05-30 19:50:20 +0800

    bug fix

--- src/app/stickynote.component.ts
+++ src/app/stickynote.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import{AppComponent} from './app.component';
 
 @Component({
    selector:'stickynote',
    template:'<h1>StickyNote</h1>'
 })
 export class StickyNoteComponent{
-    private appComponent :AppComponent;
+    
 }
