a7e783556299035dfe4c4dfd3b8536a328d282a0
adensW <1012790248@qq.com>
2017-06-06 20:06:20 +0800

    add something new but still dont konw what to do

---
+++ src/app/todocomplete.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'todocpmplete',
+  templateUrl:'./todolist.component.html' ,
+  styleUrls:['./todolist.component.css']
+})
+export class TodoListComponent {
+    
+}
